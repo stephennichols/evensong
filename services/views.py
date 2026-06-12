0478f0cfb5502b4b7e23a19ad1c6fb3b2b73fb11
Stephen Nichols <stephen.nichols@67bricks.com>
2026-06-12 12:39:24 +0100

    First frontend page for details

--- services/views.py
+++ services/views.py
@@ -4,10 +4,13 @@ from django.template import loader
 from .models import Service
 
 def index(request):
-    latest_services = Service.objects
+    latest_services = Service.objects.all()
     template = loader.get_template("services/index.html")
     context = {"latest_services": latest_services}
     return HttpResponse(template.render(context, request))
 
 def detail(request, service_id):
-    return HttpResponse("Hello. This is service %s." % service_id)
+    service = Service.objects.filter(id=service_id).first()
+    template = loader.get_template("services/details.html")
+    context = {"service": service}
+    return HttpResponse(template.render(context, request))
